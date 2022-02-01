f82965e570198be378d4c699344c5d42abc862c1
DESKTOP-SV2C925\ADMIN <allan.alzula.work@gmail.com>
2022-02-01 14:16:04 +0800

    initial functions and github validations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
+import { config as ormConfig } from './ormconfig';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, TypeOrmModule.forRoot(ormConfig)],
+  controllers: [AppController, AuthController],
+  providers: [],
 })
 export class AppModule {}
